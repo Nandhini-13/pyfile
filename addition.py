e9f54a1b2c24ae771950e5d7afdf69486eff7822
Nandhini-13 <80649359+Nandhini-13@users.noreply.github.com>
2022-05-05 14:11:53 -0500

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,10 @@
+from random import randint
+correct=0
+for i in range(20):
+    a=randint(0,100)
+    b=randint(0,100)
+    rd=a+b
+    print(a,"+",b,"=")
+    rd=int(input())
+print('your level over 20 is:',r)
+
